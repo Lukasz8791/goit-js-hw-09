15632e002e3bcc2edc1fded98aadddea358254a2
Lukasz8791 <doktor8on@gmail.com>
2023-11-27 16:49:34 +0100

    oooooooo

---
+++ src/js/02-timer.js
@@ -0,0 +1,96 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate > new Date()) {
+      startButton.disabled = false;
+    } else {
+      showAlert('Please choose a date in the future');
+      startButton.disabled = true;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+const alertElement = document.getElementById('alert');
+
+let countdownInterval;
+
+startButton.addEventListener('click', () => {
+  const selectedDate = flatpickr('#datetime-picker').selectedDates[0];
+
+  if (!selectedDate || selectedDate <= new Date()) {
+    showAlert('Please choose a valid future date');
+    return;
+  }
+
+  startButton.disabled = true;
+
+  countdownInterval = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const timeDifference = selectedDate - currentTime;
+
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      resetTimer();
+      showAlert('Countdown complete!');
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+    updateTimer(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function resetTimer() {
+  daysElement.textContent = '00';
+  hoursElement.textContent = '00';
+  minutesElement.textContent = '00';
+  secondsElement.textContent = '00';
+}
+
+function updateTimer(days, hours, minutes, seconds) {
+  daysElement.textContent = days;
+  hoursElement.textContent = formatTimeValue(hours);
+  minutesElement.textContent = formatTimeValue(minutes);
+  secondsElement.textContent = formatTimeValue(seconds);
+}
+
+function formatTimeValue(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function showAlert(message) {
+  alertElement.textContent = message;
+  alertElement.style.display = 'block';
+  setTimeout(() => {
+    alertElement.style.display = 'none';
+  }, 3000);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(form.elements.delay.value, 10);
+  const delayStep = parseInt(form.elements.step.value, 10);
+  const amount = parseInt(form.elements.amount.value, 10);
+
+  if (isNaN(firstDelay) || isNaN(delayStep) || isNaN(amount)) {
+    Notiflix.Notify.failure('Please fill in all fields with valid numbers.');
+    return;
+  }
+
+  createPromises(firstDelay, delayStep, amount);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function createPromises(firstDelay, delayStep, amount) {
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = firstDelay + (i - 1) * delayStep;
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
